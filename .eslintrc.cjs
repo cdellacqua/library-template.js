74125b6833f101de0b2d0d0e08f936d0da1eae88
cdellacqua <carlo.dellacqua97@gmail.com>
2022-04-28 21:04:28 +0200

    use stricter eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,5 +15,11 @@ module.exports = {
 	},
 	rules: {
 		"prettier/prettier": "warn",
+		"@typescript-eslint/explicit-module-boundary-types": "warn",
+		'no-shadow': 'off',
+		'@typescript-eslint/no-shadow': 'error',
+		'@typescript-eslint/no-misused-promises': 'error',
+		'@typescript-eslint/no-floating-promises': 'error',
+		'@typescript-eslint/switch-exhaustiveness-check': 'error'
 	},
 };
