f73393b1ea8e04010d31de9fa36373d73ef42c62
cdellacqua <carlo.dellacqua97@gmail.com>
2022-04-28 21:26:57 +0200

    improve comments

--- src/browser.ts
+++ src/browser.ts
@@ -1,4 +1,4 @@
-// TODO: use your library in this file, this is the script that runs in the demo page
+// TODO: use your library in this file, this is the script that runs in the demo page (npm run dev:browser)
 import {hello} from './lib';
 
 document.body.appendChild(document.createTextNode('hello, ' + hello()));
--- src/node.ts
+++ src/node.ts
@@ -1,4 +1,4 @@
-// TODO: use your library in this file, this is the script that runs in the demo page
+// TODO: use your library in this file, this is the script that runs in the demo application (npm run dev:node)
 import {hello} from './lib';
 
 console.log(hello());
