ed2b1d07427c8a7cb50aa93197ec3c80c2447699
cdellacqua <carlo.dellacqua97@gmail.com>
2022-05-21 14:42:02 +0200

    eslintrc: add project path and no-unused-vars exceptions

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,17 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:prettier/recommended',
+	],
 	plugins: ['@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2019,
+		project: 'tsconfig.json',
 	},
 	env: {
 		browser: true,
@@ -14,12 +19,16 @@ module.exports = {
 		node: true,
 	},
 	rules: {
-		"prettier/prettier": "warn",
-		"@typescript-eslint/explicit-module-boundary-types": "warn",
+		'prettier/prettier': 'warn',
+		'@typescript-eslint/explicit-module-boundary-types': 'warn',
 		'no-shadow': 'off',
 		'@typescript-eslint/no-shadow': 'error',
 		'@typescript-eslint/no-misused-promises': 'error',
 		'@typescript-eslint/no-floating-promises': 'error',
-		'@typescript-eslint/switch-exhaustiveness-check': 'error'
+		'@typescript-eslint/switch-exhaustiveness-check': 'error',
+		'@typescript-eslint/no-unused-vars': [
+			'error',
+			{varsIgnorePattern: '^_', argsIgnorePattern: '^_'},
+		],
 	},
 };
