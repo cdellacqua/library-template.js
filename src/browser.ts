9cb42b14677c6c45483c97d46c313a6b6b7b7460
cdellacqua <carlo.dellacqua97@gmail.com>
2022-06-18 10:12:10 +0200

    use appDiv in browser example; add style.css import

--- src/browser.ts
+++ src/browser.ts
@@ -1,4 +1,7 @@
+import './style.css';
 // TODO: use your library in this file, this is the script that runs in the demo page (npm run dev:browser)
 import {hello} from './lib';
 
-document.body.appendChild(document.createTextNode('hello, ' + hello()));
+const appDiv = document.getElementById('app') as HTMLDivElement;
+
+appDiv.appendChild(document.createTextNode('hello, ' + hello()));
